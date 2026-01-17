f1843ae9fbdce93cc9288ce209680debf2660d4e
= <=>
2026-01-17 18:26:04 +0100

    this is the new things

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,17 +1,29 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    // apiKey: "YOUR_API_KEY",
+    // authDomain: "YOUR_AUTH_DOMAIN",
+    // projectId: "YOUR_PROJECT_ID",
+    // storageBucket: "YOUR_STORAGE_BUCKET",
+    // messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    // appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCr7mWwOthOAGH-Q1ok7BkPBJC3l_dE0a4",
+    authDomain: "detour-7a5b4.firebaseapp.com",
+    projectId: "detour-7a5b4",
+    storageBucket: "detour-7a5b4.firebasestorage.app",
+    messagingSenderId: "781420905294",
+    appId: "1:781420905294:web:5eb7d3b851a9eca497d41f",
+    measurementId: "G-SGR49KY8KC"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
--- services/notificationService.ts
+++ services/notificationService.ts
@@ -1,3 +1,4 @@
+import Constants from 'expo-constants';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import { Platform } from 'react-native';
@@ -31,7 +32,13 @@ export const registerForPushNotificationsAsync = async () => {
             console.log('Failed to get push token for push notification!');
             return null;
         }
-        token = (await Notifications.getExpoPushTokenAsync()).data;
+        try {
+            token = (await Notifications.getExpoPushTokenAsync({
+                projectId: Constants.expoConfig?.extra?.eas?.projectId,
+            })).data;
+        } catch (error) {
+            console.log('Error fetching push token:', error);
+        }
     } else {
         console.log('Must use physical device for Push Notifications');
     }
